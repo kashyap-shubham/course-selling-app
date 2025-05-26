b545f40ce91fe1b1cb9ff6b932b1657aeb7352b0
Shubham Kashyap <74668746+kashyap-shubham@users.noreply.github.com>
2025-05-26 18:47:13 +0530

    added admin, user and course routes skeleton

--- backend/routes/routes.admin.js
+++ backend/routes/routes.admin.js
@@ -2,6 +2,27 @@ const { Router } = require("express");
 
 const adminRouter = Router();
 
+adminRouter.post("/signup", (req, res) => {
+
+})
+
+adminRouter.post("/sigin", (req, res) => {
+
+})
+
+adminRouter.post("/createCourses", (req, res) => {
+
+})
+
+adminRouter.put("/updateCourse", (req, res) => {
+
+})
+
+adminRouter.get("/createdCourses", (req, res) => {
+
+})
+
+
 module.exports = {
     adminRouter
 }
--- backend/routes/routes.user.js
+++ backend/routes/routes.user.js
@@ -1,6 +1,21 @@
 const { Router } = require("express");
-
 const userRouter = Router();
+
+
+userRouter.post("/signup", (req, res) => {
+    const { firstName, lastName, password, email } = req.body;
+    // add input validation here
+})
+
+userRouter.post("/signin", (req, res) => {
+
+})
+
+userRouter.get("/purchases", (req, res) => {
+
+})
+
+
 
 module.exports = {
     userRouter

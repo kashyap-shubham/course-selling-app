46e18ee7f26541dfde1b7c3c311b801e5c74cb2d
Shubham Kashyap <74668746+kashyap-shubham@users.noreply.github.com>
2025-05-26 18:01:27 +0530

    added the mongoose connect function and added .env.example file and added some dev and production dependencies

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,25 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
+const port = process.env.PORT || 3000;
 app.use(express.json());
 
+
+const main = () => {
+    mongoose.connect(port)
+    .then(() => {
+        console.log("Database connected successfully");
+        app.listen(port, () => {
+            console.log(`Server started at port ${port}`);
+        })
+    })
+    .catch((error) => {
+        console.log(error);
+        console.log("Db connection failed closing the server");
+        process.exit(1);
+    })
+}
+
+main();

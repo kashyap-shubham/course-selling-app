74d6155fa8420380ba1ddf6f8c16404e0c397ede
Shubham Kashyap <74668746+kashyap-shubham@users.noreply.github.com>
2025-05-27 20:38:47 +0530

    completed all the courses endpoints

--- backend/routes/routes.course.js
+++ backend/routes/routes.course.js
@@ -1,13 +1,48 @@
 const { Router } = require("express");
+const { userMiddleware } = require("../middlewares/userAuth");
+const { purchaseModel } = require("../db");
 
 const courseRouter = Router();
 
-courseRouter.post("/purchase", (req, res) => {
+courseRouter.post("/purchase", userMiddleware, async (req, res) => {
+    const userId = req.userId;
+    const courseId = req.body.courseId;
 
+    // payment verification logic goes here right now not checking for payment
+
+    try {
+        const response = await purchaseModel.create({
+            userId,
+            courseId
+        })
+
+        res.status(200).json({
+            message: "Purchase Successfull",
+            purchaseId: response._id
+        })
+
+    } catch(error) {
+        console.log(error);
+        res.json({
+            message: "Purchase Failed"
+        })
+    }
 })
 
-courseRouter.get("/preview", (req, res) => {
+courseRouter.get("/preview", async (req, res) => {
+    try {
+        const courses = await courseModel.find({});
 
+        res.status(200).json({
+           message: "All courses are",
+           courses 
+        })
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Internal Server Error"
+        })
+    }
 })
 
 
